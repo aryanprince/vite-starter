3e1d1d9925fecc02a67b3b5a4a6160c8cd66648d
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-22 22:39:33 +0000

    ✨ Setup Tailwind auto class sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold underline">
+      <h1 className="text-3xl font-bold text-red-500 underline">
         Hello world!
       </h1>
     </>
-  )
+  );
 }
